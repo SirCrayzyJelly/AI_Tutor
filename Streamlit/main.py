86514cd0a34d68cceca9a5310c2cad65ab4d6d1f
FSklepić <philippos321@gmail.com>
2025-04-14 23:32:02 +0200

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -10,7 +10,7 @@ from Backend.vektorizacija import qa_base
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Backend")))
 
 # Postavi API ključ
-API_KEY = "AIzaSyDPmp1M1bSuzAQfOuMMken6ZzzFYJ49CYM"  # <-- OBAVEZNO unesi svoj API ključ ovdje
+API_KEY = ""  # <-- OBAVEZNO unesi svoj API ključ ovdje
 genai.configure(api_key=API_KEY)
 
 # Inicijalizacija FastAPI aplikacije
